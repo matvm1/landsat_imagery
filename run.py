3f9e61fcbfa22bdcc4add873df9e997961a28902
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-01-29 12:34:00 -0500

    run.py: Imported render_template from flask and rendered index.html. Added image element to page body with jinja placeholder that receives satellite image URL from app.gee_image_service.get_satellite_image().

--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 from app import create_app
 from app.gee_image_service import initialize_gee, get_satellite_image
+from flask import render_template
 
 app = create_app()
 
@@ -7,8 +8,9 @@ app = create_app()
 @app.route("/")
 def hello_world():
     initialize_gee()
-    get_satellite_image()
-    return "<p>Hello, World!</p>"
+    city_landsat_img_url = get_satellite_image()
+    return render_template("index.html",
+                           city_landsat_img_url=city_landsat_img_url)
 
 
 if __name__ == "__main__":
