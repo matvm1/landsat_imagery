69ef63645238644e80295e2209d846050984eee0
Mateo Vergara <15144366+matvm1@users.noreply.github.com>
2025-01-29 17:40:43 -0500

    gee_image_service.get_satellite_image(): Created server proxy Number objects for lat and lon so that operations are handled server-side (GEE) when creating the region bounds.

--- app/gee_image_service.py
+++ app/gee_image_service.py
@@ -16,8 +16,8 @@ def get_satellite_image():
     # Get the coordinates of the city
     # lat, lon = get_coordinates(city_name)
 
-    lat = 40.7128
-    lon = -74.0060
+    lat = ee.Number(40.7128)
+    lon = ee.Number(-74.0060)
 
     if lat is None or lon is None:
         print(f"Could not find the coordinates for {city_name}.")
@@ -28,8 +28,8 @@ def get_satellite_image():
     # Define the region (a small rectangle around the city)
     # TODO: Refine the region based on the city's area
     region = ee.Geometry.Rectangle([
-        lon - 0.13, lat - 0.07,  # Bottom-left corner
-        lon + 0.13, lat + 0.07   # Top-right corner
+        lon.subtract(0.13), lat.subtract(0.07),  # Bottom-left corner
+        lon.add(0.13), lat.add(0.07)   # Top-right corner
     ])
 
     # Use Landsat 8 imagery (you can adjust the date range as needed)
