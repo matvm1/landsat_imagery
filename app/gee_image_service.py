036bddd22a30da65d7100d76aa6abac9e22eb095
Mateo Vergara <15144366+matvm1@users.noreply.github.com>
2025-01-28 22:27:11 -0500

    app/gee_image_service: Removed hello_gee()

--- app/gee_image_service.py
+++ app/gee_image_service.py
@@ -8,10 +8,3 @@ def initialize_gee():
         print("Google Earth Engine initialized successfully.")
     except Exception as e:
         print(f"Error initializing GEE: {e}")
-
-def hello_gee():
-    """Return a simple GEE Hello World response from the Earth Engine servers."""
-    try:
-        print(ee.String('Hello from the Earth Engine servers!').getInfo())
-    except Exception as e:
-        print(f"Error fetching message from GEE: {e}")
