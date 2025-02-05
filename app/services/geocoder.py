3fd80d5359e4d624bf471c2919bb619db6b8ab85
matvm1 <15144366+matvm1@users.noreply.github.com>
2025-02-04 23:32:15 -0500

    aervices/geocoder: Added tighter check on geocoding results

--- app/services/geocoder.py
+++ app/services/geocoder.py
@@ -9,7 +9,7 @@ def get_coords(address):
     # Geocode the address (fetch the latitude and longitude)
     geocode_result = gmaps.geocode(f"{address}")
 
-    if geocode_result:
+    if geocode_result and len(geocode_result) > 0:
         # Extract the latitude and longitude from the response
         location = geocode_result[0]['geometry']['location']
         lat = location['lat']
